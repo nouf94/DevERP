cb1ed54cbadb3fdc529b4764590f1e418fbcd712
Shahad Alkathiri <salkathiri11@gmail.com>
2021-04-25 11:21:46 +0300

    Add Impact

--- Deployment/src/main/resources/static/project/changerequest.js
+++ Deployment/src/main/resources/static/project/changerequest.js
@@ -68,23 +68,34 @@ Vue.component('myfooter', {
   '</div> '+
 '</div>'});
 
-    
+Impacts=[];
+Details=[];    
 var app = new Vue({
     el: '#nav-requests',
     data:{
       p_Description: '',
       p_Justification: '',
       p_ProjectCode: '0',
-      p_ImpactCategory: 'time',
-      p_ImpactDetails: '',
+      p_ImpactCategory: Impacts[0],
+      p_ImpactDetails: Details[0],
      Requests:'',
       errors: {
         name: false,
         email: false
       }
     },
+    mounted: function readChnages(){
+      axios.get('/rest/ReadAllChangeRequest')
+      .then(response2 => (this.Requests = response2.data,
+        ProcessRequest(this.Requests)
+          )).catch(error => {
+            console.log(error.response)
+        })
+
+    },
     methods: {
       submitValues: function (event) {
+        GetImpact();
           axios.post('/rest/CreateChangeRequest', {
             p_Description: this.p_Description,
             p_Justification: this.p_Justification,
@@ -94,26 +105,29 @@ var app = new Vue({
             }).then(response => {
               readChnages()
             }).catch(error => {
-                console.log(error.response)
+                console.log(error)
             });
-        }//End SubmitValues Method  
-      },//End  Methods
-      mounted: function readChnages(){
-        axios.get('/rest/ReadAllChangeRequest')
-        .then(response2 => (this.Requests = response2.data,
-          ProcessRequest(this.Requests)
-            )).catch(error => {
-              console.log(error.response)
-          })
+        },//End SubmitValues Method  
+        submitImpact: function (event) {
+          GetImpact();
+            axios.post('/rest/SendChangeRequestImpact', {
+              p_ImpactCategory: this.p_ImpactCategory,
+              p_ImpactDetails: this.p_ImpactDetails,
+              p_ChangeRequestCreationDate:this.p_ChangeRequestCreationDate
 
-      }
-  
+              }).then(response => {
+                console.log(response)
+              }).catch(error => {
+                  console.log(error)
+              });
+          }//End SubmitValues Method  
+      }//End  Methods
     
 });//End Vue Temp
 
 
 
-
+// Process the requests returned from backend
 function ProcessRequest(Requests){
   //Show Table of request if there is available Requests
   //Hide Message
@@ -125,8 +139,33 @@ function ProcessRequest(Requests){
     curdate.setTime(Requests[i].p_CreationDate*1000);
     Requests[i].p_CreationDate=curdate.toLocaleString();
 }
-this.$refs.Requests.refresh();
+//this.$refs.Requests.refresh();
 
 return;
 }
 
+function GetImpact(){
+ // Get All selected Impact
+// then Get Associated Details
+ $.each($("input[name='p_ImpactCategory']:checked"), function(){
+  Impacts.push($(this).val());
+  if($(this).val()=="cost"){
+    Details.push(document.getElementById('AssocCost').val);
+  }else if($(this).val()=="quality"){
+    Details.push(document.getElementById('qualityDetails').val);  
+  }
+  else if($(this).val()=="scope"){
+    Details.push(document.getElementById('scopeDetails').val); 
+  }
+  else if($(this).val()=="time"){
+    // Get Start and End Date and push to details
+    StartDate=document.getElementById('CStartDate').val;
+    EndDate=document.getElementById('CEndDate').val;
+     text = '{ "NewTimeline" : [' +
+    '{ "StartDate":"'+StartDate+'" },' +
+    '{ "EndDate":"'+EndDate+'" } ]}';
+    Details.push(text);
+  }
+});
+ return;
+}
